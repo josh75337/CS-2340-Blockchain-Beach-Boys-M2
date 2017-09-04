4938fe28deeca9a486a2d4558dc6d7a17d88e53d
Krupczak, Matthew William <mkupczak3@gatech.edu>
2017-09-04 18:31:08 -0400

    Think I got my function working. Still need to test.

--- M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -9,11 +9,12 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  *
  *  2017-09-04T18:01 Testing A basic push, here goes nothing
+ *  2017-09-04T18:09 Testing editing from emacs because I'm a scrub
  */
 public class Person4 {
   /** Holds the persons real name */
   private String name;
-    /**
+     /**
      * The constructor, takes in the persons
      * name
      * @param pname the person's real name
@@ -33,7 +34,28 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+	String outputString = new String();
+	char currentChar;
+	
+	for (int i = 0; i < outputString.length; i++) {
+	    currentChar = outputString.charAt(i);
+	    // "if current char is a number"
+	    // essentially will only modify outputString's corresponding char
+	    // if the current char has an ASCII value between '0's and '9's
+	    // i.e. it is a number.
+	    if (currentChar >= 0
+		&&
+		currentChar <= 9) {
+		//------------------
+		currentChar -= '0'; // sub. ASCII value of '0' to get raw number
+		currentChar++;      // increment the value of the number		
+		currentChar %= 10;  // ensure we wrap around to a valid number
+		                    //     if we went over
+		curretnChar += '0'; // add back ASCII value of '0' to get char
+	    }
+	    outputString += currentChar;
+	}
+	return outputString;
     }
     
     /**
